cbf94902dc0e407fe5afda9b45b3d5cd52525df4
Hasmikus <hasmikus@gmail.com>
2016-02-27 22:37:07 +0400

    Added RecyclerView project. Problem in MainActivity with MyAdapter class

---
+++ MyApplication/app/src/main/java/com/example/hasmik/myapplication/RecyclerView.java
@@ -0,0 +1,7 @@
+package com.example.hasmik.myapplication;
+
+/**
+ * Created by hasmik on 2/27/16.
+ */
+public class RecyclerView {
+}
---
+++ RecyclerView/app/src/main/java/com/example/hasmik/recyclerview/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.hasmik.recyclerview;
+
+import android.app.Activity;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+public class MainActivity extends Activity {
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+
+        mRecyclerView.setHasFixedSize(true);
+
+        String[] myDataset = {"Hi", "By"};
+        // use a linear layout manager
+        LinearLayoutManager mLayoutManager = new LinearLayoutManager(this);
+        mLayoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+
+        // specify an adapter
+        mAdapter = new MyAdapter(myDataset);
+        mRecyclerView.setAdapter(mAdapter);
+
+
+    }
+}
